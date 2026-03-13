150257425afc225ccea01570fadaa06568f9f2ba
Emilio <emilio@pi.local>
2026-03-13 03:34:56 -0700

    fix(date): parse ISO-8601 timestamps with fractional seconds

--- Nextcloud Cookbook iOS Client/Extensions/DateExtension.swift
+++ Nextcloud Cookbook iOS Client/Extensions/DateExtension.swift
@@ -28,14 +28,20 @@ extension Date {
     }
     
     static func convertISOStringToLocalString(isoDateString: String, withFormat format: String = "yyyy-MM-dd HH:mm:ss") -> String? {
-        let inputFormatter = ISO8601DateFormatter()
+        let standardFormatter = ISO8601DateFormatter()
+
+        // Some servers include fractional seconds (e.g. 2024-03-10T12:00:00.123Z)
+        // which are not parsed by default options.
+        let fractionalFormatter = ISO8601DateFormatter()
+        fractionalFormatter.formatOptions = [.withInternetDateTime, .withFractionalSeconds]
+
         // DateFormatter for converting to local time string
         let outputFormatter = DateFormatter()
         outputFormatter.dateFormat = format // You can modify this format for different output styles
         outputFormatter.timeZone = TimeZone.current // Device's local time zone
 
         // Convert the string to Date and then to local time string
-        if let date = inputFormatter.date(from: isoDateString) {
+        if let date = standardFormatter.date(from: isoDateString) ?? fractionalFormatter.date(from: isoDateString) {
             return outputFormatter.string(from: date)
         } else {
             return nil // Return nil if the input string is not in the correct format
