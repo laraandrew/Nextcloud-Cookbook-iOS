530b8aecc829666acdbbd5778450858cf83e435c
Emilio <emilio@pi.local>
2026-03-13 00:52:29 -0700

    refactor: simplify local date conversion helpers

--- Nextcloud Cookbook iOS Client/Extensions/DateExtension.swift
+++ Nextcloud Cookbook iOS Client/Extensions/DateExtension.swift
@@ -8,37 +8,29 @@
 import Foundation
 
 extension Date {
+    private static func localFormatter(withFormat format: String) -> DateFormatter {
+        let formatter = DateFormatter()
+        formatter.dateFormat = format
+        formatter.timeZone = TimeZone.current
+        return formatter
+    }
+
     static func convertUTCStringToLocalString(utcDateString: String, withFormat format: String = "yyyy-MM-dd HH:mm:ss") -> String? {
-        // DateFormatter for parsing the UTC date string
         let inputFormatter = DateFormatter()
         inputFormatter.dateFormat = format
-        inputFormatter.timeZone = TimeZone(secondsFromGMT: 0) // UTC
+        inputFormatter.timeZone = TimeZone(secondsFromGMT: 0)
 
-        // DateFormatter for converting to local time string
-        let outputFormatter = DateFormatter()
-        outputFormatter.dateFormat = format // You can modify this format for different output styles
-        outputFormatter.timeZone = TimeZone.current // Device's local time zone
-
-        // Convert the string to Date and then to local time string
-        if let date = inputFormatter.date(from: utcDateString) {
-            return outputFormatter.string(from: date)
-        } else {
-            return nil // Return nil if the input string is not in the correct format
+        guard let date = inputFormatter.date(from: utcDateString) else {
+            return nil
         }
+        return localFormatter(withFormat: format).string(from: date)
     }
-    
+
     static func convertISOStringToLocalString(isoDateString: String, withFormat format: String = "yyyy-MM-dd HH:mm:ss") -> String? {
         let inputFormatter = ISO8601DateFormatter()
-        // DateFormatter for converting to local time string
-        let outputFormatter = DateFormatter()
-        outputFormatter.dateFormat = format // You can modify this format for different output styles
-        outputFormatter.timeZone = TimeZone.current // Device's local time zone
-
-        // Convert the string to Date and then to local time string
-        if let date = inputFormatter.date(from: isoDateString) {
-            return outputFormatter.string(from: date)
-        } else {
-            return nil // Return nil if the input string is not in the correct format
+        guard let date = inputFormatter.date(from: isoDateString) else {
+            return nil
         }
+        return localFormatter(withFormat: format).string(from: date)
     }
 }
